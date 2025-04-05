f8a1d973119bccb23665660faecff788f09ee2a1
ahiaccccc <15778108169@163.com>
2025-04-05 20:12:52 +0800

    添加请求拦截器和响应拦截器

--- qihuang-client/src/utils/http.js
+++ qihuang-client/src/utils/http.js
@@ -9,4 +9,29 @@ const httpInstance = axios.create({
   // }
 })
 
+httpInstance.interceptors.request.use(config => {
+  // const userStore = useUserStore();
+  // const token = userStore.userInfo.accessToken;
+  // if(token){
+      // config.headers.Authorization = `Bearer ${token}`
+  // }
+  return config
+}, error => {
+  return Promise.reject(error);
+})
+
+httpInstance.interceptors.response.use(res =>res.data,e=>{
+  if(e.response.status === 401){
+    // Handle 401 Unauthorized error, e.g., redirect to login page
+    // const router = useRouter()
+    // router.push({ name: 'login' })
+    // localStorage.removeItem('token')
+    // location.reload()
+  }
+
+  return Promise.reject(e)
+
+})  
+
 export default httpInstance
+
