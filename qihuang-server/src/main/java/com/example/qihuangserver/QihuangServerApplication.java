77b2f2485d60291d0f97c0d19614f392d55d568b
ahiaccccc <15778108169@163.com>
2025-04-05 21:50:48 +0800

    新建后端项目

---
+++ qihuang-server/src/main/java/com/example/qihuangserver/QihuangServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.qihuangserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QihuangServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QihuangServerApplication.class, args);
+    }
+
+}
